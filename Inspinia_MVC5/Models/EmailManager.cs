1559245c3285ae77c9084d0e8be01faec34b915d
estherjacobs <esther@1on1development.com>
2016-06-20 16:43:48 -0400

    Updated Email Manager to send emails to correct address

--- Inspinia_MVC5/Models/EmailManager.cs
+++ Inspinia_MVC5/Models/EmailManager.cs
@@ -51,13 +51,13 @@ namespace Inspinia_MVC5.Models
             //var template = File.ReadAllText("WelcomeEmail.cshtml");
             //var result = Engine.Razor.RunCompile(template, "templateKey", null, new { Name = name });
             var fromAddress = new MailAddress("expirationtracking@gmail.com", "Expiration Tracking App");
-            var toAddress = new MailAddress(/*u.User.Email*/ "esther@1on1development.com", "To " + name);
+            var toAddress = new MailAddress(email, "To " + name);
             const string fromPassword = "Esther110";
             string subject = "Welcome to Expiration Tracking App";
             string body = "Hi " + name + ", Welcome to Expiration Tracking App. You’ve officially taken the first step toward the pleasure of sitting back knowing your expiration dates are handled. You’ll have more accountability, a happier and more productive team, and save time and frustration. Our Tracking tool is incredibly easy-to-use and easy-to-learn." +
             " View the entire list of expiration items, change expiration dates, add new ones, attach documents and images related to the item and feel free that everything will" +
             " be tracked and properly notified.If you run into any snags along the way, let us know.We’re here to help. Don’t hesitate to email us if you have any questions." +
-            " Happy expiration tracking,      Expiration Tracking App;)      expirationtracking @gmail.com";
+            " Happy expiration tracking,      Expiration Tracking App;)      expirationtracking@gmail.com";
 
             var smtp = new SmtpClient
             {
@@ -80,7 +80,7 @@ namespace Inspinia_MVC5.Models
         public void SendInvitationMemberEmail(string email, string token, Organization o)
         {
             var fromAddress = new MailAddress("expirationtracking@gmail.com", "Expiration Tracking App");
-            var toAddress = new MailAddress(/*u.User.Email*/ "esther@1on1development.com", "To new member");
+            var toAddress = new MailAddress(email, "To new member");
             const string fromPassword = "Esther110";
             string subject = "Invitation to create user";
             string body = "Hi, you have been invited by " + o.Name + " to become a member of their organization at Expiration Tracking App. Follow this link to setup your account. http://localhost:58893/members/link?token=" + token;
